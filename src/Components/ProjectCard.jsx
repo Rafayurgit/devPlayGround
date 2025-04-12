0f91828d4ffe20189aa3ceca38bde624ae3e62b7
farruop <rafayKhanpvt@gmail.com>
2025-04-13 01:43:58 +0530

    Add: cardComponent and basic app.jsx structure

---
+++ src/Components/ProjectCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+function ProjectCard({project}) {
+  return (
+    <Link to={project.path}>
+        <div className="bg-white p-6 rounded-xl shadow hover:shadow-lg transition">
+            <h2 className="text-xl font-semibold">{project.name}</h2>
+            <p className="text-gray-600">{project.description}</p>
+        </div>
+    </Link>
+  )
+}
+
+export default ProjectCard
