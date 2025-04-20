a891788eecd6a5559d0bb72ad2e034aae5387ce5
farruop <rafayKhanpvt@gmail.com>
2025-04-20 16:15:42 +0530

    Add: used local storage to store todos

--- src/Projects/ToDo/Todo.jsx
+++ src/Projects/ToDo/Todo.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Todo.css";
 
 
@@ -10,6 +10,8 @@ const Todo = () => {
 
   const [editIndex, seteditIndex]= useState("")
   const [editText,  setEditText] =useState("")
+
+  const [isLoading, setIsLoading] = useState(false);
 
   // ✅ Function to handle input change
 
@@ -24,26 +26,24 @@ const Todo = () => {
     setTodo("")
   }
 
-  
+  // ✅ Optional: Function to delete or toggle a todo item
+
   const handelRemove=( index)=>{
     return setTodoList((prev)=> prev.filter((prev, idx)=> idx!==index)  )
 
     // setTodoList((prev)=>{
     //   return prev.filter((item, idx)=> idx!==index)
     // })
+
   }
 
   const handelUpdate =(index, todo)=>{
     seteditIndex(index)
     setEditText(todo)
-
-    
   }
 
- 
-
   const handelSave =(index, todo)=>{
-    setTodoList((prevTodo)=> prevTodo.map((prev, idx)=> idx === index ? todo : prev))
+    setTodoList((prevTodo)=> prevTodo.map((prev, idx)=> idx === index ? todo.trim() : prev))
     seteditIndex("")
     setEditText("")
     
@@ -54,9 +54,20 @@ const Todo = () => {
     seteditIndex("")
   }
 
-  // ✅ Optional: Function to delete or toggle a todo item
+    useEffect (()=>{
+      if(isLoading){
+        localStorage.setItem("todo-item", JSON.stringify(todoList))
+      }
+    }, [todoList, isLoading])
 
+    useEffect(()=>{
+      const storedTodo= localStorage.getItem("todo-item")
+      if(!storedTodo) return
+      setTodoList( JSON.parse(storedTodo))
+      setIsLoading(true)
+    },[])
 
+  
   return (
     
     
@@ -89,11 +100,11 @@ const Todo = () => {
         />  
         {/* ✅ Add Button */}
         <button type="button" 
-        className= "add-button" 
+        className= "add-button hover:bg-gray-500 " 
         onClick={handelAdd}
         disabled={!todo.trim()}
         >Add</button></>
-          )}
+        )}
         
         
         </div>        
@@ -105,10 +116,12 @@ const Todo = () => {
             <li key={index} className="todo-item" >
               {editIndex===index ? (
                 <div>
-                  <span><input type="text" value={editText} onChange={(e)=>setEditText(e.target.value)}/></span>
-                  <div>
-                    <button onClick={()=>handelSave(index, editText)}>save</button>
-                    <button onClick={()=>handelCancel(index)}>cancel</button>
+                  <span className="todo-input-group">
+                    <input className="todo-input"  type="text" value={editText} onChange={(e)=>setEditText(e.target.value)}/>
+                  </span>
+                  <div className="todo-actions">
+                    <button className="update-button" onClick={()=>handelSave(index, editText)}>save </button>
+                    <button className="remove-button" onClick={()=>handelCancel(index)}>cancel</button>
                   </div>
                 </div>
               )
