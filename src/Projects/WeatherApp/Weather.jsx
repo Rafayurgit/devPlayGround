dbb5f2e733a506d687730616a258767823a5142e
farruop <rafayKhanpvt@gmail.com>
2025-04-26 18:26:30 +0530

    Add: weather app dependencied and structure

--- src/Projects/WeatherApp/Weather.jsx
+++ src/Projects/WeatherApp/Weather.jsx
@@ -1,37 +1,44 @@
 import React, { useState } from "react";
 
 
+const api_key= import.meta.env.VITE_API_KEY;
+
+
+
 export default function Weather(){
 
     const [city, setCity] =useState("")
 
-    const handelSearch =()=>{
+    const handelSearch = async(city_name)=>{
         
         try {
-            const data = fetch("https://openweathermap.org/api")
-            res.status(200).json(data, "Data fetched")
+            const response =await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${api_key}`)
+            const data =await response.json();
+            console.log(data);
+            
 
         } catch (error) {
-            res.status(500).json("Internal server error")
+            console.log("Internal server error",error);
+            
         }
     }
 
     return(
         <>
 
-        <div >
-            <div className="input-box">
-                <input className="input" type="text" 
+        <div className="">
+            <div className="input-box p-5 ">
+                <input className="input border text-black rounded p-3 " type="text" 
                 onChange={(e)=>setCity(e.target.value)}
                 />
             </div>
 
             <div>
-                <button onClick={handelSearch}>Search</button>
+                <button className="bg-blue-700 rounded px-5 py-1 text-white hover:bg-blue-300 cursor-pointer" onClick={()=>{handelSearch(city)}}>Search</button>
             </div>
 
             <div>
-                <div>Result</div>
+                <h3 className="  ">Result</h3>
             </div>
         </div>
 
