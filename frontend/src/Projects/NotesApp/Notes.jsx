25218d63ce01d43686171190919cbc96ea730e7c
farruop <rafayKhanpvt@gmail.com>
2025-05-04 00:35:15 +0530

    Add: add topics functionality

--- frontend/src/Projects/NotesApp/Notes.jsx
+++ frontend/src/Projects/NotesApp/Notes.jsx
@@ -4,19 +4,36 @@ export default function Notes(){
 
 
 const [note, setNote]= useState({title: "", content: ""});
+const [topicInput, setTopicInput]= useState("");
+const [topics, setTopics] = useState([])
 const [noteList, setNoteList] = useState([]);
 
+
 const handelAdd=()=>{
-    setNoteList((prev)=>[...prev, note])
-    setNote({title:"", content:""})
+    if(note.title.trim() && note.content.trim() ){
+        const newNote={...note, topics: [...topics]}
+
+        setNoteList((prev)=>[...prev, newNote])
+        setNote({title:"", content:""})
+        setTopics([]);
+    }
+    
+    
 }
 
-
+const handelTopics =()=>{
+    if(topicInput.trim().length>0){
+        setTopics((prev)=> [...prev, topicInput.trim()])
+        setTopicInput("")
+    }
+    
+}
 
     return(
         <>
         <div>
-            <div className="p-3">
+            <div>
+                <div className="p-3">
                 <input className="border-2 p-3 rounded-r-2xl"
                 type="text"  placeholder="title"
                 value={note.title} 
@@ -31,9 +48,30 @@ const handelAdd=()=>{
             </div>
 
             <div>
-                <button className=" px-3 py-1 rounded-lg bg-blue-500 cursor-pointer hover:bg-blue-300 " type="button"
-                onClick={handelAdd}>ADD Note</button>
+                <input className="border-2 p-3 rounded-r-2xl"
+                type="text" placeholder="topics"
+                value={topicInput}
+                onChange={(e)=>setTopicInput(e.target.value)}
+                />
             </div>
+
+        </div>
+            
+        <div className="">
+            
+            <div className="p-3">
+                <button className=" px-3 py-1 rounded-lg bg-blue-500 cursor-pointer hover:bg-blue-300 " type="button"
+                onClick={handelAdd}>
+                Add Note</button>
+            </div>
+
+            <div className="p-3">
+                <button className="px-3 py-1 rounded-lg bg-blue-500 cursor-pointer hover:bg-blue-300 " type="button"
+                onClick={handelTopics}
+                 >Add Topics</button>
+            </div>
+        </div>
+            
 
 
             <div>
@@ -42,6 +80,16 @@ const handelAdd=()=>{
                         <strong> {note.title}</strong>: {note.content}
                     </li>
                 ))}
+                </ul>
+            </div>
+
+            <div>
+                <ul>
+                   {topics.map((topic, idx)=>(
+                    <li key={idx}>
+                        {topic}
+                    </li>
+                   ))} 
                 </ul>
             </div>
         </div>
