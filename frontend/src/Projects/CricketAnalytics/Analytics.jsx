da9db79fb0f1848d86a0d16fcc78547d4ce6ff9e
farruop <rafayKhanpvt@gmail.com>
2025-05-10 23:40:14 +0530

    Add: basic dropdown selector

--- frontend/src/Projects/CricketAnalytics/Analytics.jsx
+++ frontend/src/Projects/CricketAnalytics/Analytics.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 export default function Analytics (){
+
+    const [selectYear, setSelectYear] = useState("");
+
+    const handelYearSelect =(e)=>{
+        setSelectYear(e.target.value)
+    }
+
     return(
         <div>
-            See Analytics
+            <h1>See you fav match analytics</h1>
+
+
+            <div>
+                <select name="yearSelect" id=""  onChange={handelYearSelect}>
+                    <option value="">Select an option</option>
+                    <option value="2016">2016</option>
+                    <option value="2017">2017</option>
+                </select>
+
+                <p>Selected year: {selectYear}</p>
+                
+            </div>
         </div>
     )
 }
