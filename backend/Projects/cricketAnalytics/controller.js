9481b2065ec04ab7ff4c606b1cb01b089f100cd5
farruop <rafayKhanpvt@gmail.com>
2025-06-02 22:57:09 +0530

    Re-structuring project for clean code

---
+++ backend/Projects/cricketAnalytics/controller.js
@@ -0,0 +1,20 @@
+import { getTopCities } from "./utilities/csvProcessor";
+import { getFourNsix } from "./utilities/csvProcessor2";
+
+export const handelTopCities = async (req, res)=>{
+    try {
+        const result = await getTopCities();
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json({error: "Failed to process csv", detail: error.message})
+    }
+}
+
+export const handleTopScore = async(req,res)=>{
+    try {
+        const result = await getFourNsix();
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json({error: "Failed to process csv", detail: error.message})
+    }
+}
---
+++ backend/Projects/cricketAnalytics/utilities/csvProcessor2.js
@@ -0,0 +1,58 @@
+import fs from "fs";
+import csvParser from "csv-parser";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirName= dirname(__filename);
+
+const matchesPath = path.join(__dirName, "../data/matches.csv");
+const deliveriesPath = path.join(__dirName, "../data/deliveries.csv");
+
+export const getFourNsix =()=>{
+    return new Promise((resolve, reject)=>{
+
+        const matchSeasonMap={};
+        const finalResult={};
+
+        fs.createReadStream(matchesPath)
+        .pipe(csvParser())
+        .on("data", (data)=>{
+            matchSeasonMap[data.ID]=data.SEASON;
+        })
+        .on("end", ()=>{
+            fs.createReadStream(deliveriesPath)
+            .pipe(csvParser())
+            .on("data", (data)=>{
+                const matchId= data.MATCH_ID;
+                const season = matchSeasonMap[matchId];
+                const team = data.BATTING_TEAM;
+
+                if(!season) return;
+
+                if(!finalResult[season]) finalResult[season]={};
+                if(!finalResult[season][team]){
+                    finalResult[season][team]={
+                        count4:0,
+                        count6:0,
+                        countRun:0
+
+                    }
+                }
+
+                if(data.BATTING_RUNS ==="4"){
+                    finalResult[season][team].count4 += 1;
+                }
+                if(data.BATTING_RUNS ==="6"){
+                    finalResult[season][team].count6 += 1;
+                }
+
+                finalResult[season][team].countRun += parseInt(data.TOTAL_RUNS,10)
+            })
+            .on("end", ()=>{
+                resolve(finalResult)
+            })
+            .on("error", (err)=>reject(err));
+        }).on("error", (err)=>reject(err));
+    })
+}
