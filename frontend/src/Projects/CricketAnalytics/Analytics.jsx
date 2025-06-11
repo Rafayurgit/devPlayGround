034828accc5cc791e491c9a9dbf9d21de2e3bc5b
farruop <rafayKhanpvt@gmail.com>
2025-06-12 01:46:50 +0530

    Add: fetch data from backend url endpoint

--- frontend/src/Projects/CricketAnalytics/Analytics.jsx
+++ frontend/src/Projects/CricketAnalytics/Analytics.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Analytics() {
   const [selectYear, setSelectYear] = useState("");
@@ -7,11 +7,25 @@ export default function Analytics() {
     setSelectYear(e.target.value);
   };
 
+  useEffect(()=>{
+  
+  fetch("http://localhost:8080/api/cricket-analytics/top-cities")
+    .then(res=> res.json())
+    .then(data=> console.log("Data from backend", data))
+    .catch(err=> console.log("Backend Error", err));
+
+  fetch("http://localhost:8080/api/cricket-analytics/top-score")
+    .then(res=>res.json())
+    .then(data=> console.log("Data from Backend", data))
+    .catch(err=> console.log("Error from backend", err))
+
+  },[])
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-6">
       <h1 className="text-3xl font-semibold text-indigo-600 mb-6">See your fav match analytics</h1>
 
-      <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-sm">
+      <div className=" bg-white p-6 rounded-lg shadow-lg w-full max-w-sm">
         <h3 className="text-xl font-medium text-gray-700 mb-4">Select the year</h3>
         <select
           name="yearSelect"
@@ -24,7 +38,19 @@ export default function Analytics() {
           <option value="2017">2017</option>
         </select>
 
-        <p className="mt-4 text-gray-700">Selected year: {selectYear}</p>
+        <select
+          name="yearSelect"
+          id=""
+          onChange={handelYearSelect}
+          className="w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
+        >
+          <option value="">Select an option</option>
+          <option value="2016">Top-Cities</option>
+          <option value="2017">Top-Score</option>
+        </select>
+
+        <p className="mt-4 text-gray-700"> Selected year: {selectYear}</p>
+        <p></p>
       </div>
     </div>
   );
