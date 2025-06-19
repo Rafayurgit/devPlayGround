3a0b84cbc8b51cc074f200d26f57530bd7eb1ebf
farruop <rafayKhanpvt@gmail.com>
2025-06-20 00:22:21 +0530

    Add: routes and schema for pollmaste

--- backend/Database/Models/pollsSchema.js
+++ backend/Database/Models/pollsSchema.js
@@ -1,14 +1,65 @@
-const mongoose = require("mongoose");
+import mongoose from 'mongoose';
 
 const pollSchema = new mongoose.Schema({
   question: {
     type: String,
-    required: true
+    required: true,
+    trim: true
   },
-  options: {
-    type: [String], // Array of strings like ["React", "Vue"]
-    required: true
+  option1: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  option2: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  option3: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  option4: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  option1Votes: {
+    type: Number,
+    default: 0
+  },
+  option2Votes: {
+    type: Number,
+    default: 0
+  },
+  option3Votes: {
+    type: Number,
+    default: 0
+  },
+  option4Votes: {
+    type: Number,
+    default: 0
+  },
+  option1Percentage: {
+    type: Number,
+    default: 0
+  },
+  option2Percentage: {
+    type: Number,
+    default: 0
+  },
+  option3Percentage: {
+    type: Number,
+    default: 0
+  },
+  option4Percentage: {
+    type: Number,
+    default: 0
   }
+}, {
+  versionKey: false
 });
 
-module.exports = mongoose.model("Poll", pollSchema);
+export default mongoose.model("Polls", pollSchema);
--- backend/Projects/pollMaster/routes.js
+++ backend/Projects/pollMaster/routes.js
@@ -3,19 +3,77 @@ import pollsSchema from "../../Database/Models/pollsSchema";
 
 const router= express.Router();
 
-router.put("/createRouter", async(req,res)=>{
+router.put("/polls/create", async(req,res)=>{
     try {
         const {question, option1,option2,option3,option4} = req.body;
+
         if(!question.trim() || !option1.trim() || !option2.trim() || !option3.trim() || !option4.trim()){
             return res.status(400).json({error: "Question can't be empty and all options must be filled"});
         }
 
-        const newPoll = new pollsSchema({question, option1,option2,option3,option4});
+        // const newPoll = new pollsSchema({question, option1,option2,option3,option4});
+        await pollsSchema.deleteMany();
+        const newPoll = new pollsSchema({ question, option1, option2, option3, option4 });
         await newPoll.save();
-        res.status(201).json({"Successfully created poll": newPoll})
+        res.status(201).json({message: "Successfully created poll"})
     } catch (error) {
         res.status(400).json({"Failed to create poll": error.message})
     }
 })
+
+
+router.get("/polls/fetch", async(req,res)=>{
+    try {
+
+        const polls= await pollsSchema.find();
+        if(!polls || polls.length===0){
+           return res.status(400).json({message:"Poll not found create one first"})
+        }
+
+        res.status(200).json({message: "POll data fetched from collection", data: polls})
+
+        
+    } catch (error) {
+        res.status(400).json({error: "something went wrong"})
+    }
+})
+
+
+router.patch("/polls/updateVotes", async(req,res)=>{
+    try {
+        const  selectedOptions = req.body;
+        const validOptions =[option1,oprion2,option3,option4];
+        if(!validOptions.includes(selectedOptions)){
+            return res.status(200).json({message:"Invalid option selected"})
+        }
+
+        const voteCount= pollsSchema.findOne();
+        if(!voteCount) return res.status(400).json({error: "Poll not found"})
+
+        const voteField= `${selectedOptions}votes`;
+        voteCount[voteField]+=1;
+
+        const totalVotes = voteCount.option1+ voteCount.option2+ voteCount.option3+ voteCount.option4;
+
+        voteCount.option1Percentage= Number(((poll.option1vote/totalVotes)*100).toFixed(2))
+        voteCount.option2Percentage= Number(((poll.option2vote/totalVotes)*100).toFixed(2))
+        voteCount.option3Percentage= Number(((poll.option3vote/totalVotes)*100).toFixed(2))
+        voteCount.option4Percentage= Number(((poll.option4vote/totalVotes)*100).toFixed(2))
+
+        await voteCount.save();
+
+        // voteCount.map((vote, idx)=>{
+        //     if(vote.option===option1) voteCount[idx]+=1;
+        //     elseif(vote.option===option2) voteCount[idx]+=1;
+        //     elseif(vote.option===option3) voteCount[idx]+=1;
+        //     else voteCount[idx]+=1;
+        // })
+        res.status(200).json({message:"Vote register successfully"})
+    } catch (error) {
+        res.status(400).json({error:"error.message"})
+    }
+})
+
+
 
 export default router;
