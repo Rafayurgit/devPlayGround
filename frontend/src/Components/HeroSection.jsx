a520f182fbab322c2d2626df88e87f3580e8f9a9
farruop <rafayKhanpvt@gmail.com>
2025-06-24 23:13:14 +0530

    Add: update herosection and skill page

--- frontend/src/Components/HeroSection.jsx
+++ frontend/src/Components/HeroSection.jsx
@@ -1,19 +1,16 @@
-import React from 'react'
 
 function HeroSection() {
+
   return (
     <div>
-      <section
-              id="hero"
-              className="snap-start h-screen flex items-center justify-center px-4"
-            >
-              <div className="w-full max-w-5xl h-[80vh] border border-white/10 rounded-xl backdrop-blur-md shadow-lg p-10 flex flex-col items-center justify-center">
-                <h1 className="text-4xl text-white font-bold">Hero Section</h1>
-                <p className="text-white/70 mt-4">Welcome to the portfolio</p>
-                
-                
-              </div>
-        </section>
+      <section className="snap-start h-screen w-full flex items-center justify-center px-8">
+        <div className="w-full h-full flex flex-col items-center justify-center text-white text-center">
+          <h1 className=" text-5xl font-bold mb-4 top-80 left-50">A.RAFAY KHAN 🌍</h1>
+          <p className=" text-lg max-w-2xl text-white/70 left-50">
+          I am just a guy who is vlogger,developer,designer and devops engineer for fun.
+          </p>
+        </div>
+      </section>
     </div>
   )
 }
--- frontend/src/Components/SkillSection.jsx
+++ frontend/src/Components/SkillSection.jsx
@@ -1,13 +1,36 @@
 import React from 'react'
+import {SiReact, SiTailwindcss, SiJavascript, SiMongodb, SiGithub, SiLinkedin} from "react-icons/si"
 
 function SkillSection() {
   return (
     <div>
       <section
               id="hero"
-              className="snap-start h-screen flex items-center justify-center px-4"
+              className="relative snap-start h-screen flex items-center justify-center px-4"
+              
             >
-              <div className="w-full max-w-5xl h-[80vh] border border-white/10 rounded-xl backdrop-blur-md shadow-lg p-10 flex flex-col items-center justify-center">
+              {/* <img src="../public/vite.svg" alt="" className='absolute top-50 left-70 w-20 h-20 z-100 drop-shadow-[0_0_10px_#61dafb] hover:scale-110 transit cursor-pointer animate-bounceSlow'/> */}
+              <a href="" target='_blank' rel='noopener noreferre'>
+              <SiReact className="text-cyan-400 drop-shadow-[0_0_10px_#61dafb] w-20 h-20 absolute top-30 left-170  hover:scale-110 cursor-pointer animate-bounceSlow z-10" />
+              </a>
+
+              <a href="https://github.com/Rafayurgit" target='_blank' rel='noopener noreferre'>
+              <SiGithub className="text-purple-950 drop-shadow-[0_0_10px_#8b5cf6] w-20 h-20 absolute top-50 right-64  hover:scale-110 cursor-pointer animate-bounceSlow  z-10" />
+              </a>
+
+              <a href="https://www.linkedin.com/in/rafay-khan-li" target='_blank' rel='noopener noreferre'>  
+              <SiLinkedin className="text-blue-700 drop-shadow-[0_0_10px_#1d4ed8] w-20 h-20 absolute top-50 left-70  hover:scale-110 cursor-pointer animate-bounceSlow  z-10" />                
+              </a>
+
+
+              <div className="w-full max-w-5xl h-[80vh] border border-white/10 rounded-xl backdrop-blur-md shadow-lg p-10 flex flex-col items-center justify-center bg-cover bg-center bg-no-repeat"
+              style={{backgroundImage: "linear-gradient(rgba(20,0,40,0.7), rgba(10,0,50,0.7)), url(../public/herobg.png)",
+                filter: "grayscale(100%)"
+              }}
+              >
+                <h1 className="text-4xl text-white font-bold">My skillset</h1>
+                <p className="text-white/70 mt-4">I do video editing too</p>
+                
                 
               </div>
         </section>
